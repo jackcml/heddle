57283596e8d9b5585f9e9241386557e467d73ad8
Jack Lualdi <jacklualdi@gmail.com>
2026-06-18 16:45:54 -0400

    feat: add argparse and route to repl/file runner

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
+from argparse import ArgumentParser
+
+def run_file(filename: str) -> None:
+    print('TODO: File input not yet implemented.')
+
+
+def run_repl() -> None:
+    print('TODO: REPL not yet implemented.')
+
+
 def main():
-    print("Hello from heddle!")
+    parser = ArgumentParser(
+            prog='heddle',
+            description='A DSL for image and video transformation.',
+            epilog='Sources in `./` are available as global variables (without extensions).')
+    parser.add_argument('filename', nargs='?', help='input file; otherwise reads from stdin')
+    args = parser.parse_args()
+
+    if args.filename:
+        run_file(args.filename)
+    else:
+        run_repl()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
